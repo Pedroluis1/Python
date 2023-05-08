ee342e549caa52818e3292a59f3ba03ec225c82e
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2023-05-08 15:00:00 -0300

    commit

--- mundo 3/des096.py
+++ mundo 3/des096.py
@@ -1,2 +1,10 @@
-for i in range(90, 39, -10):
-    print(i)
+def area (a, l):
+    ar = l*a
+    print(f'A área de um terreno {l} x {a} é de {ar}m².')
+
+
+print('Controle de Terrenos')
+print('-'*30)
+la = float(input('LARGURA (m): '))
+al = float(input('COMPRIMENTO (m): '))
+area(l=la, a=al)
--- mundo 3/des098.py
+++ mundo 3/des098.py
@@ -1,3 +1,32 @@
-def soma (n1,n2):
-    resultado = n1+n2
-    return resultado
+from time import sleep
+def contagem(i, f, p):
+    if p == 0:
+        p = 1
+    if i and f > 0 > p:
+        p = p + (2*(-p)) # 40 50 -1 ( convertendo para o positivo)
+        print(p)
+    sleep(0.5)
+    print(f'Contagem de {i} até {f-1} de {p} em {p}')
+    if i > f:
+        p = -p #50 10 (convertendo para o negativo)
+        f = f -2
+    if f<0:
+        p = -p # (convertendo para o negativo) fim: -10
+    if i > 0 > f:
+        p = -p #?? converter (adapitando) quando 90 -40 10 e vice versa!! 
+    for c in range (i, f, p):
+        sleep(0.5)
+        print( f'{c} ', end='')
+    print('FIM! ')
+    print('-='*20)
+
+
+print('-='*20)
+contagem(1, 11, 1)
+contagem(10, 1, 2)
+print('Agora é a sua vez de personalizar a contagem!')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))+1
+passo = int(input('Passo: '))
+print('-='*20)
+contagem(inicio, fim, passo)

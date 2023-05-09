206b8bfeca46990f0389845ff7ab8457dc28b563
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2023-05-09 17:14:17 -0300

    Update des101.py

--- mundo 3/des101.py
+++ mundo 3/des101.py
@@ -1,1 +1,13 @@
-help(print)
+from datetime import date
+def voto (ano):
+    print(f'Com {date.today().year - ano} anos: ', end='')
+    if date.today().year - ano > 18:
+        print('VOTO OBRIGATÓRIO.')
+    elif date.today().year - ano < 18:
+        print('NÃO VOTA.')
+    else:
+        print('VOTO OPCIONAL.')
+
+
+ano = int(input(30*'-','\nEm que ano você nasceu? '))
+voto(ano)

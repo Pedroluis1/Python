ded564465639a34f649158f15f34916d203c3f2c
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2023-05-10 12:49:04 -0300

    Update des102.py

--- mundo 3/des102.py
+++ mundo 3/des102.py
@@ -1,4 +1,4 @@
-def fatorial (num):
+def fatorial(num, show=False):
     """
     fatorial(n, show=False)
         -> calcula o fatorial de um número.
@@ -7,9 +7,16 @@ def fatorial (num):
         :param return: O valor do fatorial de um número n.
     """
     f = 1
-    for c in range (num, 0, -1):
+    for c in range(num, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
         f *= c
-        return f
+    return f
 
-
-print(fatorial(5))
+#programa principal
+print(fatorial(5, show=True))
+#help(fatorial)

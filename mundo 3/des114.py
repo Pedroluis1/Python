62f051c2155449324923447ed4788a1b21871685
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2023-05-25 10:18:08 -0300

    commit

--- mundo 3/des114.py
+++ mundo 3/des114.py
@@ -2,4 +2,8 @@ import urllib
 import urllib.request
 
 try:
-    site = urllib.request.urlopen()
+    site = urllib.request.urlopen('https://www.github.com')
+except:
+    print('Deu erro')
+else:
+    print('tudo okay')

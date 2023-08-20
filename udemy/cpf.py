2aec262fcdcdc106c403fcce1dcd6cae33afe28b
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2023-08-20 01:43:38 -0300

    commit

--- udemy/cpf.py
+++ udemy/cpf.py
@@ -2,42 +2,42 @@ from random import randint
 
 def cpf():
     cont_regr = 10
-    cpf_usuário = []
+    cpf_usuario = []
     digito = 0
     a = 9
 
     while True:
         for i in range (0,a):
             if a == 9:
-                cpf_usuário.append(randint(0,9))
-            digito += (cpf_usuário[i] * cont_regr)
+                cpf_usuario.append(randint(0,9))
+            digito += (cpf_usuario[i] * cont_regr)
             cont_regr -= 1
             if cont_regr == 1:
                 digito *= 10
                 digito %= 11
                 digito = digito if digito <= 9 else 0
-                cpf_usuário.append(digito)
+                cpf_usuario.append(digito)
                 digito = 0
                 if a == 10:
                     cont = 0
                     for item in range (0, 12):
                         if cont == 6 or cont == 3:
-                            print(f'.{cpf_usuário[item]}',end='')
+                            print(f'.{cpf_usuario[item]}',end='')
                         else:
-                            print(cpf_usuário[item],end='')
+                            print(cpf_usuario[item],end='')
                         cont += 1
                         if cont == 9:
                             break
-                    print(f'-{cpf_usuário[9]}{cpf_usuário[10]}')
+                    print(f'-{cpf_usuario[9]}{cpf_usuario[10]}')
                     break
                 a = 10
                 cont_regr = 11
 
 
 def validador (*num):
-    cpf_usuário = ''.join(num)
+    cpf_usuario = ''.join(num)
     valida = []
-    for item in cpf_usuário:
+    for item in cpf_usuario:
         valida.append(int(item))
     conf = [valida.pop(-2), valida.pop(-1)]
 
@@ -64,26 +64,27 @@ def validador (*num):
                 a = 10
                 cont_regr = 11
 
+
 def cpf_retorna():
     cont_regr = 10
-    cpf_usuário = ""
+    cpf_usuario = ""
     digito = 0
     a = 9
 
     while True:
         for i in range(a):
             if a == 9:
-                cpf_usuário += str(randint(0, 9))
-            digito += int(cpf_usuário[i]) * cont_regr
+                cpf_usuario += str(randint(0, 9))
+            digito += int(cpf_usuario[i]) * cont_regr
             cont_regr -= 1
             if cont_regr == 1:
                 digito *= 10
                 digito %= 11
                 digito = digito if digito <= 9 else 0
-                cpf_usuário += str(digito)
+                cpf_usuario += str(digito)
                 digito = 0
                 if a == 10:
-                    return cpf_usuário
+                    return cpf_usuario
                 a = 10
                 cont_regr = 11
 
